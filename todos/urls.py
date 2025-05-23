03c9abe1b7a5ba5e1206a16a84bfd367968c049f
abionajoshua1 <abionajoshua411@gmail.com>
2025-05-23 12:55:45 +0100

    modified:   db.sqlite3 	modified:   todos/admin.py 	modified:   todos/templates/todos/home.html 	modified:   todos/templates/todos/login.html 	modified:   todos/templates/todos/logout.html 	modified:   todos/urls.py 	modified:   todos/views.py

--- todos/urls.py
+++ todos/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from todos import views
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
+from  todos.views import custom_logout_view, login_view, profile_view
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('mark_completed/<int:todo_id>/', views.mark_completed, name='mark_completed'),
     path('profile/', views.profile_view, name='profile'), 
-    path('logout/', auth_views.LogoutView.as_view(template_name='todos/logout.html'), name='logout'),
+    path('logout/',views.custom_logout_view,  name='logout'),
+    path('login/', views.login_view, name='login'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 from .forms import TodoForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     if request.method == 'POST':
@@ -25,3 +27,23 @@ def mark_completed(request, todo_id):
 def profile_view(request):
     return render(request, 'profile.html')
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # change to your actual home route
+        else:
+            messages.error(request, 'Invalid username or password')
+    
+    return render(request, 'todos/login.html')
+
+def custom_logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return render(request, 'todos/logout.html')  # render your logged out page
+    else:
+        return redirect('home')

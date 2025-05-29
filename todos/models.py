003216713a612b235d129dadc5e93838aa3d0387
abionajoshua1 <abionajoshua411@gmail.com>
2025-05-29 11:18:38 +0100

    modified:   db.sqlite3 	new file:   todos/migrations/0002_todo_user.py 	modified:   todos/models.py

--- todos/models.py
+++ todos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +11,4 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.title
+    
